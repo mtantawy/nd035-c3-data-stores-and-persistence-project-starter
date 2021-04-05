7992aa4403f4b094b4ce34a091c4c26f1073502e
Mahmoud Tantawy <me@mtantawy.com>
2021-04-05 14:21:11 +0200

    DB Config & Entities

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -0,0 +1,74 @@
+package com.udacity.jdnd.course3.critter.pet;
+
+import com.udacity.jdnd.course3.critter.user.Customer;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PetType type;
+
+    private String name;
+
+    @ManyToOne
+    private Customer owner;
+
+    private LocalDate birthDate;
+
+    private String notes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/Customer.java
@@ -0,0 +1,65 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Nationalized
+    private String name;
+
+    private String phoneNumber;
+
+    private String notes;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Pet> pets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+}
